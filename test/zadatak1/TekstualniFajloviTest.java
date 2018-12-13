c0d571b6b4b8c828eb34fa88873a576e609e1112
Nikola Milikic <nikola.milikic@gmail.com>
2018-12-13 16:16:14 +0100

    Sredjeni testovi za klasu TekstualniFajlovi.

--- test/zadatak1/TekstualniFajloviTest.java
+++ test/zadatak1/TekstualniFajloviTest.java
@@ -26,13 +26,17 @@ public class TekstualniFajloviTest {
 	@Test
 	public void metoda_ucitajIVratiTekst() {
 		try {
-			TekstualniFajlovi.upisiTekst("tekst.txt");
-			String ucitaniTekst = TekstualniFajlovi.ucitajIVratiTekst("tekst.txt");
-			String ocekivaniTekst = "Danas je lep dan. Mozda ce temperatura biti iznad nule. ";
-			assertEquals("Nije upisan ocekivani tekst", ocekivaniTekst, ucitaniTekst);		
-			File fajl = new File("tekst.txt");
-			fajl.delete();			
+			String tekst = "Danas je lep dan.\nMozda ce temperatura biti iznad nule.";
+			String imeFajla = "tekst.txt";
 			
+			upisiTekst(imeFajla, tekst);
+			
+			String ucitaniTekst = TekstualniFajlovi.ucitajIVratiTekst(imeFajla);
+			
+			assertEquals("Nije upisan ocekivani tekst.", tekst+"\n", ucitaniTekst);		
+			
+			File fajl = new File(imeFajla);
+			fajl.delete();	
 		} catch (IOException e) {
 			fail("Greska prilikom ucitavanja teksta iz fajla tekst.txt proverite da li postoji fajl i da li ste prosledili dobro ime fajla metodi");
 		}		
@@ -41,14 +45,23 @@ public class TekstualniFajloviTest {
 	@Test
 	public void metoda_upisiTekst() {
 		try {
-			TekstualniFajlovi.upisiTekst("tekst2.txt");
-			String ucitaniTekst = TekstualniFajlovi.ucitajIVratiTekst("tekst2.txt");
-			String ocekivaniTekst = "Danas je lep dan. Mozda ce temperatura biti iznad nule. ";
-			assertEquals("Nije upisan ocekivani tekst", ocekivaniTekst, ucitaniTekst);			
+			String imeFajla = "tekst2.txt";
+			
+			TekstualniFajlovi.upisiTekst(imeFajla);
+			String ucitaniTekst = TekstualniFajlovi.ucitajIVratiTekst(imeFajla);
+			String ocekivaniTekst = "Danas je lep dan.\nMozda ce temperatura biti iznad nule.\n";
+			assertEquals("Nije upisan ocekivani tekst", ocekivaniTekst, ucitaniTekst);
+			
+			File fajl = new File(imeFajla);
+			fajl.delete();
 		} catch (IOException e) {
 			fail("Greska prilikom upisivanja teksta iz fajla tekst2.txt proverite da li postoji fajl i da li ste prosledili dobro ime fajla metodi");
 		}
 	}
 	
-
+	private void upisiTekst(String imeFajla, String tekst) throws IOException {
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(imeFajla)));
+		out.println(tekst);
+		out.close();
+	}
 }	
